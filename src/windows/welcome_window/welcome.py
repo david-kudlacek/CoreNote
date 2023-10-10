2f1f1e52261d0b4e5be45c931fe26fb3384e663e
David Kudláček <david.kudlacek.it@gmail.com>
2023-10-10 19:34:55 +0200

    Update welcome.py

--- src/windows/welcome_window/welcome.py
+++ src/windows/welcome_window/welcome.py
@@ -3,28 +3,26 @@ from PySide6 import QtCore as qtc
 from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 
-# Name of automatically-generated class from welcome_window.py
 from welcome_window import Ui_w_WelcomeForm
 
 
-# Class inheriting from QWidget and aforementioned automatically-generated class
 class WelcomeForm(qtw.QWidget, Ui_w_WelcomeForm):
 
     welcome_finished = qtc.Signal()
 
     def __init__(self):
-        super().__init__()  # Initialize parent class; QWidget
+        super().__init__()  # Initialize QWidget
         self.setupUi(self)
 
-        self.pb_continue.clicked.connect(self.get_started)  # Fun to run when pb is clicked
+        self.pb_continue.clicked.connect(self.get_started)
 
     @qtc.Slot()
-    def get_started(self):  # User clicks on the get started button
+    def get_started(self):  # User clicks on the "Get Started" pb
         self.welcome_finished.emit()
         self.close()
 
 
-if __name__ == "__main__":  # Create WelcomeForm class and display window to user
+if __name__ == "__main__":  # Create instance and display window
     app = qtw.QApplication(sys.argv)
 
     window = WelcomeForm()
